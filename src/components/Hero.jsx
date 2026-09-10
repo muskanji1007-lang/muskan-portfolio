c195975ddc2ebb336b3ce020bcaa512e644b07d6
muskanji1007-lang <muskanji1007@gmail.com>
2026-09-11 00:19:58 +0530

    Initial portfolio setup

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+function Hero() {
+  return (
+    <section id="home">
+      <h1>Muskan Gupta</h1>
+      <p>I am a Computer Science and engineering student in data Science, 
+        interseted in web development and learning new technology .</p>
+         <h3>Frontend Developer</h3>
+         <button>View My Work</button>
+    </section>
+  )
+}
+
+export default Hero
